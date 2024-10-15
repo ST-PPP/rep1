06198ba0df8903284de081c69053554954e38802
ST-PPP <150949703+ST-PPP@users.noreply.github.com>
2024-10-16 00:17:37 +0300

    Delete project directory

--- project/simpleapp/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimpleappConfig(AppConfig):
-    verbose_name = 'Публикации'
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'simpleapp'
